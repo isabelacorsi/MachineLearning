ba079abf215544076d73b79015b4d03f2b4ad496
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 18:06:06 -0300

    Create PreProcessamento_CreditData.py

---
+++ Pré-processamento de Dados/PreProcessamento_CreditData.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+base = pd.read_csv('credit_data.csv')
+
+base.mean()
+base['age'].mean()
+base['age'][base.age > 0].mean()
+base.loc[base.age < 0, 'age'] = 40.92
+
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
+
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
+
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
