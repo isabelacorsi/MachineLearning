2a4cc2676ece834560a12ddd58becfbd9bd1edb2
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 18:16:15 -0300

    Create RegressaoLogistica_Census.py

---
+++ Regressão Logistica/RegressaoLogistica_Census.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+import collections
+
+base = pd.read_csv('census.csv')
+
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
+previsores = column_tranformer.fit_transform(previsores).toarray()
+
+labelencorder_classe = LabelEncoder()
+classe = labelencorder_classe.fit_transform(classe)
+
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+classificador = LogisticRegression(solver='lbfgs')
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+collections.Counter(classe_teste)
+
+print(precisao)
+print(matriz)
