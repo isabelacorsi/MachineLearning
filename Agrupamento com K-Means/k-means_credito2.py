d65770f137d80fa527bb20478a943117367aba6a
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 17:48:08 -0300

    Add files via upload

--- Agrupamento com K-Means/k-means_credito2.py
+++ Agrupamento com K-Means/k-means_credito2.py
@@ -1,13 +1,13 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 import numpy as np
 
 base = pd.read_csv('credit_card_clients.csv', header = 1)
 base['BILL_TOTAL'] = base['BILL_AMT1'] + base['BILL_AMT2'] + base['BILL_AMT3'] + base['BILL_AMT4'] + base['BILL_AMT5'] + base['BILL_AMT6']
 
-X = base.iloc[:,[1,25]].values
+X = base.iloc[:,[1,2,3,4,5,25]].values
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
@@ -19,18 +19,10 @@ for i in range(1, 11):
 plt.plot(range(1, 11), wcss)
 plt.xlabel('Número de clusters')
 plt.ylabel('WCSS')
+plt.show()
 
 kmeans = KMeans(n_clusters = 4, random_state = 0)
 previsoes = kmeans.fit_predict(X)
-
-plt.scatter(X[previsoes == 0, 0], X[previsoes == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[previsoes == 1, 0], X[previsoes == 1, 1], s = 100, c = 'orange', label = 'Cluster 2')
-plt.scatter(X[previsoes == 2, 0], X[previsoes == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[previsoes == 3, 0], X[previsoes == 3, 1], s = 100, c = 'blue', label = 'Cluster 4')
-plt.xlabel('Limite')
-plt.ylabel('Gastos')
-plt.legend()
-plt.show()
 
 lista_clientes = np.column_stack((base, previsoes))
 lista_clientes = lista_clientes[lista_clientes[:,26].argsort()]
