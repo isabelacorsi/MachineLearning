5631e52d8cbf78db21ff614dbc4122ff6aa5a9f2
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 17:56:33 -0300

    Add files via upload

--- Aprendizagem por Árvore de Decisão/RandomForest_CreditData.py
+++ Aprendizagem por Árvore de Decisão/RandomForest_CreditData.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 base = pd.read_csv('credit_data.csv')
@@ -12,23 +12,18 @@ base.loc[base.age < 0, 'age'] = 40.92
 previsores = base.iloc[:, 1:4].values
 classe = base.iloc[:, 4].values
 
-
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer = imputer.fit(previsores[:, 1:4])
 previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
 
-
 scaler = StandardScaler()
 previsores = scaler.fit_transform(previsores)
 
-
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
 
-
-classificador = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classificador = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
 classificador.fit(previsores_treinamento, classe_treinamento)
 previsoes = classificador.predict(previsores_teste)
-
 
 precisao = accuracy_score(classe_teste, previsoes)
 matriz = confusion_matrix(classe_teste, previsoes)
