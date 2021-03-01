93ddf89854ac700d9b9f72a286d51ecab49b146c
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 18:11:18 -0300

    Add files via upload

--- Regressão Linear/RegressaoLinear_Simples_Casas.py
+++ Regressão Linear/RegressaoLinear_Simples_Casas.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
 base = pd.read_csv('house_prices.csv')
 
-X = base.iloc[:, 3:19].values
+X = base.iloc[:, 5:6].values
 y = base.iloc[:, 2].values
 
 X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(X, y,
@@ -14,13 +16,18 @@ regressor = LinearRegression()
 regressor.fit(X_treinamento, y_treinamento)
 score = regressor.score(X_treinamento, y_treinamento)
 
+plt.scatter(X_treinamento, y_treinamento)
+plt.plot(X_treinamento, regressor.predict(X_treinamento), color = 'red')
+
 previsoes = regressor.predict(X_teste)
 
+resultado = abs(y_teste - previsoes)
+resultado.mean()
+
 mae = mean_absolute_error(y_teste, previsoes)
+mse = mean_squared_error(y_teste, previsoes)
+
+plt.scatter(X_teste, y_teste)
+plt.plot(X_teste, regressor.predict(X_teste), color = 'red')
 
 print(regressor.score(X_teste, y_teste))
-
-regressor.intercept_
-len(regressor.coef_)
-
-
