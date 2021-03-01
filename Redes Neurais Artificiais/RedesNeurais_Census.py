557517ac1f558abcc8c2e22a5e8c9b7c6381c2be
Isabela Corsi <isabelacorsi@icloud.com>
2021-03-01 18:09:17 -0300

    Create RedesNeurais_Census.py

---
+++ Redes Neurais Artificiais/RedesNeurais_Census.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+base = pd.read_csv('census.csv')
+
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
+previsores = column_tranformer.fit_transform(previsores).toarray()
+
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
+
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+classificador = MLPClassifier(verbose = True, max_iter=1000, tol=0.000010)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+print(classificador.out_activation_)
+
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+print(precisao)
+print(matriz)
